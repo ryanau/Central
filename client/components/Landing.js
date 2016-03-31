0774fe85e9068a0053bdb41dbcc5501a19c7a49f
ryanau <ryan.au@hotmail.com>
2016-03-31 09:48:05 -0700

    separate out navbar from landing js; boostrap auth.js

--- client/components/Landing.js
+++ client/components/Landing.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router';
 
 import VolunteerSignUpBox from 'components/VolunteerSignUpBox';
+import NavBar from 'components/NavBar';
 
 import ApiConstants from 'api_constants';
 import ApiRequests from 'api_requests';
@@ -32,48 +32,14 @@ class Landing extends React.Component {
   componentWillUnmount() {
     MasterStore.unlisten(this.onChange);
   }
-  _onSignOutClickedAdmin = () => {
-    const resolve = (res) => {
-      localStorage.clear()
-      window.location.href = '/'
-    }
-    ApiRequests.del(ApiConstants.session.admin_sign_out, null, resolve)
-  }
-  _onSignOutClicked = () => {
-    const resolve = (res) => {
-      localStorage.clear()
-      window.location.href = '/'
-    }
-    ApiRequests.del(ApiConstants.session.sign_out, null, resolve)
-  }
   _onChange(state) {
     this.setState(state);
   }
   render() {
-    let userName, signOutLink, eventNav, signOutLinkAdmin, signInLink, signInLinkAdmin
-    if (this.state.loggedIn && this.state.authorization == 'user') {
-      userName = this.state.user.uid;
-      signOutLink = (<li><input type="button" onClick={this._onSignOutClicked} value="Sign out"/></li>)
-      eventNav = (<li><Link to="/user/events">Events</Link></li>)
-    } else if (this.state.loggedIn && this.state.authorization == 'admin') {
-      userName = this.state.user.uid;
-      signOutLinkAdmin = (<li><input type="button" onClick={this._onSignOutClickedAdmin} value="Admin Sign out"/></li>);
-      eventNav = (<li><Link to="/admin/events">Events</Link></li>)
-    } else {
-      signInLink = (<li><Link to="/auth">Sign Up/In</Link></li>)
-      signInLinkAdmin = (<li><Link to="/admin_auth">Admin Sign Up/In</Link></li>)
-    }
     return (
       <div>
+        <NavBar user={this.state.user} loggedIn={this.state.loggedIn} authorization={this.state.authorization}/>
         <h1>Welcome to Central</h1>
-        {userName}
-        <ul>
-          {signInLink}
-          {signInLinkAdmin}
-          {signOutLink}
-          {signOutLinkAdmin}
-          {eventNav}
-        </ul>
         <VolunteerSignUpBox/>
         {this.props.children}
       </div>
---
+++ client/components/NavBar.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+import ApiConstants from 'api_constants';
+import ApiRequests from 'api_requests';
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onChange = this._onChange.bind(this);
+  }
+  componentWillMount() {
+    this.setState({
+      user: this.props.user,
+      loggedIn: this.props.loggedIn,
+      authorization: this.props.authorization,
+    })
+  }
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      user: nextProps.user,
+      loggedIn: nextProps.loggedIn,
+      authorization: nextProps.authorization,
+    })
+  }
+  _onSignOutClickedAdmin = () => {
+    const resolve = (res) => {
+      localStorage.clear()
+      window.location.href = '/'
+    }
+    ApiRequests.del(ApiConstants.session.admin_sign_out, null, resolve)
+  }
+  _onSignOutClicked = () => {
+    const resolve = (res) => {
+      localStorage.clear()
+      window.location.href = '/'
+    }
+    ApiRequests.del(ApiConstants.session.sign_out, null, resolve)
+  }
+  _onChange(state) {
+    this.setState(state);
+  }
+  render() {
+    let userName, signOutLink, eventNav, signOutLinkAdmin, signInLink, signInLinkAdmin
+    if (this.state.loggedIn && this.state.authorization == 'user') {
+      userName = this.state.user.uid;
+      signOutLink = (<li><input type="button" onClick={this._onSignOutClicked} value="Sign out"/></li>)
+      eventNav = (<li><Link to="/user/events">Events</Link></li>)
+    } else if (this.state.loggedIn && this.state.authorization == 'admin') {
+      userName = this.state.user.uid;
+      signOutLinkAdmin = (<li><input type="button" onClick={this._onSignOutClickedAdmin} value="Admin Sign out"/></li>);
+      eventNav = (<li><Link to="/admin/events">Events</Link></li>)
+    } else {
+      signInLink = (<li><Link to="/auth">Sign Up/In</Link></li>)
+      signInLinkAdmin = (<li><Link to="/admin_auth">Admin Sign Up/In</Link></li>)
+    }
+    return (
+      <div>
+        <ul>
+          {userName}
+          {signInLink}
+          {signInLinkAdmin}
+          {signOutLink}
+          {signOutLinkAdmin}
+          {eventNav}
+        </ul>
+      </div>
+    );    
+  }
+}
+
+export default NavBar;
