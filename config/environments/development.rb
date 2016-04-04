539ea143d279a51eadf8e7d43503e33c3257aedc
ryanau <ryan.au@hotmail.com>
2016-04-04 00:07:15 -0700

    change seed; take out strip in sms inbound

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,4 +42,27 @@ Rails.application.configure do
   # Configuring letter opener in dev mode
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
+
+
+  # action mailer config with sendgrid
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # ActionMailer::Base.smtp_settings = {
+  #   :address        => "smtp.sendgrid.net",
+  #   :port           => "25",
+  #   :authentication => :plain,
+  #   :user_name      => ENV['SENDGRID_USERNAME'],
+  #   :password       => ENV['SENDGRID_PASSWORD'],
+  #   :domain         => ENV['SENDGRID_DOMAIN']
+  # }
+
+  # config.action_mailer.smtp_settings = {
+  #       :address              => "smtp.gmail.com",
+  #       :port                 => 587,
+  #       :domain               => "gmail.com",
+  #       :user_name            => ENV['GMAIL_USERNAME'],
+  #       :password             => ENV['GMAIL_PASSWORD'],
+  #       :authentication       => :plain,
+  #       :enable_starttls_auto => true,
+  # }
+  # config.action_mailer.default_url_options = { :host => "localhost:3000" }
 end
