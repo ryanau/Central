0452030fcb91c2af835f110022b64929ddbfd408
ryanau <ryan.au@hotmail.com>
2016-04-04 01:06:17 -0700

    configure sendgrid gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,33 +40,6 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # Configuring letter opener in dev mode
-  # config.action_mailer.delivery_method = :letter_opener
-  # config.action_mailer.default_url_options = { :host => "localhost:3000" }
-
-
-  # action mailer config with sendgrid
-  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  # ActionMailer::Base.smtp_settings = {
-  #   :address        => "smtp.sendgrid.net",
-  #   :port           => "25",
-  #   :authentication => :plain,
-  #   :user_name      => ENV['SENDGRID_USERNAME'],
-  #   :password       => ENV['SENDGRID_PASSWORD'],
-  #   :domain         => ENV['SENDGRID_DOMAIN']
-  # }
-
-  # Send emails in test mode
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  
-  config.action_mailer.smtp_settings = {
-        :address              => "smtp.gmail.com",
-        :port                 => 587,
-        :domain               => "gmail.com",
-        :user_name            => ENV['GMAIL_USERNAME'],
-        :password             => ENV['GMAIL_PASSWORD'],
-        :authentication       => :plain,
-        :enable_starttls_auto => true,
-  }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
 end
