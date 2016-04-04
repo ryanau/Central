b8034ae23bef30a6cf9b27f6057e6683484408db
ryanau <ryan.au@hotmail.com>
2016-04-04 10:45:38 -0700

    rename routes to router

--- client/module.js
+++ client/module.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, browserHistory } from 'react-router';
-import routes from 'routes';
+
+import router from 'router';
 import './assets/app.css';
 import './assets/fixed-data-table.min.css'
 
-ReactDOM.render(<Router history={browserHistory} routes={routes}/>, document.getElementById('content'))
+ReactDOM.render(router, document.getElementById('content'))
--- client/router.js
+++ client/router.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, IndexRedirect } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 
 // System
 import Root from 'components/Root';
@@ -10,6 +11,8 @@ import Auth from 'components/session/Auth';
 import AdminAuth from 'components/session/AdminAuth';
 import AccountActivation from 'components/session/AccountActivation';
 import AuthComplete from 'components/session/AuthComplete';
+import UserSignOut from 'components/session/UserSignOut';
+import AdminSignOut from 'components/session/AdminSignOut';
 
 // Admin
 import AdminEventsContainer from 'components/admin/events/EventsContainer';
@@ -24,27 +27,31 @@ import UserReport from 'components/user/reports/UserReport';
 import UserTask from 'components/user/tasks/UserTask';
 
 
-
-const routes = (
+const router = (
+  <Router history={browserHistory}>
   <Route path='/' component={Root}>
   	<Route path='auth' component={Auth}/>
   	<Route path='admin_auth' component={AdminAuth}/>
     <Route path='account_activation' component={AccountActivation}/>
+    <Route path='auth_complete' component={AuthComplete}/>
   	<Route path='auth_complete' component={AuthComplete}/>
   	<Route path='admin'>
+      <Route path='sign_out' component={AdminSignOut}/>
 	  	<Route path='events' component={AdminEventsContainer}/>
 			<Route path='events/:eventId' component={AdminEvent}/>
 			<Route path='events/:eventId/reports/:reportId' component={AdminReport}/>
   	</Route>
     <Route path='user'>
-      <Route path='dashboard' component={UserDashboard}/>
+      <Route path='sign_out' component={UserSignOut}/>
       <Route path='events' component={UserEventsContainer}/>
       <Route path='events/:eventId' component={UserEvent}/>
       <Route path='events/:eventId/reports/:reportId' component={UserReport}/>
       <Route path='events/:eventId/tasks/:taskId' component={UserTask}/>
     </Route>
+    <Route path='dashboard' component={UserDashboard}/>
     <Route path="*" component={NoMatch}/>
   </Route>
+  </Router>
 );
 
-export default routes;
+export default router;
